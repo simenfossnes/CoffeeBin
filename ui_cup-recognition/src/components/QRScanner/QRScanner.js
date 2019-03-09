c6609d4fa680421100d11577d410dd4b95d4af27
Simen Fossnes <simenf13@student.uia.no>
2019-03-09 02:57:42 +0100

    Updated gitignore. Added firebae deployment config. Updated QRScanner to receive video stream from the phone's camera (chrome support only).

--- ui_cup-recognition/src/components/QRScanner/QRScanner.js
+++ ui_cup-recognition/src/components/QRScanner/QRScanner.js
@@ -8,12 +8,50 @@ import styles from "./styles.module.css";
 // some native apis like the camera.
 
 // could also use the <input type="file" accept="image/*" capture="camera">
+// https://stackoverflow.com/questions/8581081/how-to-access-a-mobiles-camera-from-a-web-app
 // tag, but this would make the experience alot less smooth.
 // need to think and discuss this a bit more.
+
+// ok, there is an actual solution to this. wow.
+// https://youtu.be/z9unKFzAj1w?t=1904
 class QRScanner extends React.Component {
-  render() {
-    return <div className={styles.QRScannerContainer}>test</div>;
+  
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+        this.canvasRef = React.createRef();
+    }
+
+    async componentDidMount() {
+        this.videoRef.current.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
+        await this.videoRef.current.play();
+        this.draw();
+    }
+  
+    render() {
+    return (
+      <div className={styles.QRScannerContainer}>
+        <video className={styles.video} ref={this.videoRef} muted style={{ display: "none" }} />
+        <canvas className={styles.canvas} ref={this.canvasRef} />
+      </div>
+    );
   }
+
+  draw = () => {
+      requestAnimationFrame(this.draw);
+
+      const context = this.canvasRef.current.getContext('2d');
+      // Draw video frame
+      context.drawImage(this.videoRef.current, 0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
+  }
+
+//   async getUserMedia() {
+//     // get camera stream  
+//     this.videoRef.current.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
+//     await this.videoRef.current.play();
+//     this.draw();
+//   }
 }
+
 
 export default QRScanner;
