ad6135cb545831dd6ff8dbd0cebdc57b7a74487d
Simen Fossnes <simenf13@student.uia.no>
2019-03-09 05:02:31 +0100

    QR Scanner working - detecting the QR codes, and reading out the data!

--- ui_cup-recognition/src/components/QRScanner/QRScanner.js
+++ ui_cup-recognition/src/components/QRScanner/QRScanner.js
@@ -1,5 +1,7 @@
 import React from "react";
+import jsQR from "jsqr";
 import styles from "./styles.module.css";
+
 
 // hmm, seems like we need access to the phone camera
 // directly here to get a stream. This could be an issue
@@ -15,42 +17,69 @@ import styles from "./styles.module.css";
 // ok, there is an actual solution to this. wow.
 // https://youtu.be/z9unKFzAj1w?t=1904
 class QRScanner extends React.Component {
+
+    state = {
+        qrDetected: false,
+    }
   
     constructor(props) {
         super(props);
         this.videoRef = React.createRef();
         this.canvasRef = React.createRef();
+        this.setInterval = undefined;
     }
 
     async componentDidMount() {
         this.videoRef.current.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
         await this.videoRef.current.play();
+        this.setInterval = setInterval(this.detectQRCode, 3000);
         this.draw();
     }
   
     render() {
+        const { qrDetected } = this.state;
     return (
       <div className={styles.QRScannerContainer}>
-        <video className={styles.video} ref={this.videoRef} muted style={{ display: "none" }} />
+        {qrDetected && <h1>aorise ntoaresi tn</h1>}
+        <video className={styles.video} ref={this.videoRef} muted />
         <canvas className={styles.canvas} ref={this.canvasRef} />
       </div>
     );
   }
 
+  componentWillUnmount() {
+    clearInterval(this.setInterval);
+}
+
   draw = () => {
-      requestAnimationFrame(this.draw);
+    requestAnimationFrame(this.draw);
 
       const context = this.canvasRef.current.getContext('2d');
       // Draw video frame
       context.drawImage(this.videoRef.current, 0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
   }
 
-//   async getUserMedia() {
-//     // get camera stream  
-//     this.videoRef.current.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
-//     await this.videoRef.current.play();
-//     this.draw();
-//   }
+  detectQRCode = () => {
+
+    const w = this.videoRef.current.videoWidth;
+    const h = this.videoRef.current.videoHeight;
+
+    this.canvasRef.current.width = w;
+    this.canvasRef.current.height = h;
+
+    const ctx = this.canvasRef.current.getContext('2d');
+    ctx.drawImage(this.videoRef.current, 0, 0, w, h);
+    const imageData = ctx.getImageData(0, 0, w, h).data;
+
+    const code = jsQR(imageData, this.videoRef.current.videoWidth, this.videoRef.current.videoHeight);
+    if (!code) {
+        if (this.state.qrDetected) this.setState({qrDetected: false});
+        return;
+    }
+
+    this.setState({qrDetected: true});
+    // trigger API call here
+  }
 }
 
 
